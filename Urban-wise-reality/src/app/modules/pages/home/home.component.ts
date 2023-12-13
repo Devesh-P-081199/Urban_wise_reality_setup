e419d6f055d5bb89f007916bea36a0c25cc79963
mondayishere01 <deveshpandey.pear@gmail.com>
2023-12-13 13:25:10 +0530

    added some stuff

--- Urban-wise-reality/src/app/modules/pages/home/home.component.ts
+++ Urban-wise-reality/src/app/modules/pages/home/home.component.ts
@@ -9,6 +9,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class HomeComponent implements OnInit {
   form!: FormGroup;
   active_tab: number = 0;
+  otpGenerated: any;
+  imageList: any;
+  videoList: any;
 
   constructor(private fb: FormBuilder) {}
 
@@ -29,14 +32,27 @@ export class HomeComponent implements OnInit {
       expected_price: ['', Validators.required],
       expected_deposit: ['', Validators.required],
       expected_rent: ['', Validators.required],
-      property_size: ['', Validators.required],
+      room_number: ['', Validators.required],
+      room_size: ['', Validators.required],
+      lot_size: ['', Validators.required],
+      image_upload: ['', Validators.required],
+      video_upload: ['', Validators.required],
     });
   }
   sendData() {
     console.log(this.form.value);
+    this.form.reset();
   }
 
   switchTab(res: any) {
     this.active_tab = res;
+  }
+
+  send_image_data(res: any, type: boolean) {
+    if (type) {
+      this.imageList = res.target.files;
+    } else {
+      this.videoList = res.target.files;
+    }
   }
 }
